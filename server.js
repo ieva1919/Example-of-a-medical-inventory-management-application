34e0bbf41825ec2262c1f941f9c41c548c671e79
Ieva Sinkeviciute <ieva1919@gmail.com>
2020-12-13 19:01:23 +0200

    add new some packages, add med form, set up database, submit form date

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require('express')
+const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
 
 const app = express()
+const mustache = mustacheExpress()
+mustache.cache = null
+app.engine('mustache', mustache)
+app.set('view engine', 'mustache')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/add', (req, res)=> {
+    res.render('med-form')
+})
+
+app.post('/meds/add', (req, res)=> {
+    console.log('post body', req.body)
+    res.redirect('/meds')
+})
+
+app.get('/meds', (req, res)=> {
+    res.render('meds')
+})
 
 app.listen(5001,() => {
     console.log('Listening to port 5001')
 } )
+
+// continue with line 215
